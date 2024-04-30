a2868360034ea8c5dc6c65d4102f1bf07afe0cf6
malfaro21 <malfaro21@students.desu.edu>
2024-04-29 20:10:48 -0400

    added to server.py

---
+++ server.py
@@ -0,0 +1,13 @@
+import socket
+import sys
+import threading
+
+
+IP = '127.0.0.1'  
+PORT = 12000  
+
+def start_server(ip, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((ip, port))
+    server_socket.listen(1)
+    print(f'Server ready and listening on {ip}:{port}')
